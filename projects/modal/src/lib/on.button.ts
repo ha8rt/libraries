d2131a709e7cbdbbe136d453a21f000e2fd9f8ed
Tamás Varró <varro.tamas@szegedsw.hu>
2019-09-01 21:06:18 +0200

    modal 0.0.6

---
+++ projects/modal/src/lib/on.button.ts
@@ -0,0 +1,12 @@
+import { Service, Body } from '@ha8rt/http.service';
+
+export function onButton(service: Service, event: Body, callback: (data) => void) {
+   const button = event.getKey();
+   if (button === 'add-ok') {
+      service._post(event, callback);
+   } else if (button === 'edit-ok') {
+      service._patch(event, callback);
+   } else if (button === 'del-ok') {
+      service._delete(event, callback);
+   }
+}
--- projects/modal/src/public-api.ts
+++ projects/modal/src/public-api.ts
@@ -2,9 +2,7 @@
  * Public API Surface of modal
  */
 
-export * from './lib/modal.service';
-export * from './lib/modal.component';
-export * from './lib/modal.module';
 export * from './lib/modal.body';
 export * from './lib/modal.button';
 export * from './lib/modal.handler';
+export * from './lib/on.button';
