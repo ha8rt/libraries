27cc1dd0092f531981a428227e93fb9183f06d59
Tamás Varró <varro.tamas@szegedsw.hu>
2019-09-21 10:23:54 +0200

    select 0.0.2

--- projects/select/src/lib/select.handler.ts
+++ projects/select/src/lib/select.handler.ts
@@ -1,5 +1,6 @@
 import { ElementRef } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { getFieldValue } from '@ha8rt/table';
 
 export type ElemList = IElem[];
 
@@ -15,4 +16,26 @@ export function getControl(elemList: ElemList, name: string): FormControl {
 
 export function getElement(elemList: ElemList, name: string): ElementRef<any> {
    return elemList.find((value) => value.name === name).element;
+}
+
+export function getNative(elemList: ElemList, name: string) {
+   return getElement(elemList, name).nativeElement;
+}
+
+export function getInvalid(elemList: ElemList): boolean {
+   let invalid = false;
+   elemList.forEach(elem => {
+      if (elem.control.invalid && elem.control.touched) {
+         invalid = true;
+      }
+   });
+   return invalid;
+}
+
+export function getValue(elemList: ElemList, name: string, rows: any[], code: string): any {
+   return getFieldValue(rows[getControl(elemList, name).value], code);
+}
+
+export function getIndexOfRow(rows: any[], row: any, code: string) {
+   return rows.findIndex((value) => getFieldValue(value, code) === getFieldValue(row, code));
 }
