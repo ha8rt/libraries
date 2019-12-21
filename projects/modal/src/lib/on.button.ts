97bbad46667dd14bda61ec299d626a98cae59daa
Tamás Varró <varro.tamas@szegedsw.hu>
2019-12-21 19:21:23 +0100

    modal 0.0.26

--- projects/modal/src/lib/on.button.ts
+++ projects/modal/src/lib/on.button.ts
@@ -1,6 +1,6 @@
-import { Service, Body } from '@ha8rt/http.service';
+import { Body, HttpService } from '@ha8rt/http.service';
 
-export function onButton(service: Service, event: Body, callback: (data: any) => void) {
+export function onButton(service: HttpService, event: Body, callback: (data: any) => void) {
    const button = event.getKey();
    if (button === 'add-ok') {
       service._post(event, callback);
