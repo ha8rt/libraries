85390a3a497fb7c721e8299710c7c20b484b874b
Tamás Varró <varro.tamas@szegedsw.hu>
2020-01-24 10:30:45 +0100

    icon 0.0.15

--- projects/icon/src/lib/icon.component.ts
+++ projects/icon/src/lib/icon.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 // tslint:disable-next-line: max-line-length
 import { faComment as farComment, faCommentDots as farCommentDots, faComments as farComments, faEdit as farEdit } from '@fortawesome/free-regular-svg-icons';
 // tslint:disable-next-line: max-line-length
-import { faBox, faCheck, faChevronDown, faChevronUp, faClipboardCheck, faClipboardList, faCogs, faComment, faCommentDots, faComments, faDollarSign, faDolly, faDownload, faEdit, faGlobeAmericas, faHome, faInfo, faInfoCircle, faKey, faPlus, faPrint, faReceipt, faReplyAll, faSearch, faShare, faSignOutAlt, faThumbtack, faTimes, faTrashAlt, faTruck, faUndoAlt, faUserEdit, faUsersCog, faWarehouse } from '@fortawesome/free-solid-svg-icons';
+import { faBox, faCheck, faChevronDown, faChevronUp, faClipboardCheck, faClipboardList, faCogs, faComment, faCommentDots, faComments, faDollarSign, faDolly, faDownload, faEdit, faGlobeAmericas, faHome, faInfo, faInfoCircle, faKey, faPlus, faPrint, faReceipt, faReplyAll, faSearch, faShare, faSignOutAlt, faThumbtack, faTimes, faTrashAlt, faTruck, faUndoAlt, faUserEdit, faUsersCog, faWarehouse, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
    selector: 'lib-icon',
@@ -16,7 +16,7 @@ export class IconComponent {
 
    fas = {
       // tslint:disable-next-line: max-line-length
-      faPlus, faCheck, faEdit, faTimes, faDownload, faComment, faComments, faCommentDots, faDollarSign, faSignOutAlt, faKey, faTrashAlt, faClipboardCheck, faClipboardList, faDolly, faThumbtack, faTruck, faCogs, faBox, faInfo, faInfoCircle, faPrint, faGlobeAmericas, faHome, faUsersCog, faWarehouse, faSearch, faChevronDown, faChevronUp, faUserEdit, faUndoAlt, faReceipt, faShare, faReplyAll
+      faPlus, faCheck, faEdit, faTimes, faDownload, faComment, faComments, faCommentDots, faDollarSign, faSignOutAlt, faKey, faTrashAlt, faClipboardCheck, faClipboardList, faDolly, faThumbtack, faTruck, faCogs, faBox, faInfo, faInfoCircle, faPrint, faGlobeAmericas, faHome, faUsersCog, faWarehouse, faSearch, faChevronDown, faChevronUp, faUserEdit, faUndoAlt, faReceipt, faShare, faReplyAll, faSyncAlt
    };
    far = { farEdit, farComment, farComments, farCommentDots };
 
