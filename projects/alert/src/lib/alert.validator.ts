a4a8006d5f2d91f5c63b4c0e3390fc5d6d2f4136
ha8rt <varro.tamas@szegedsw.hu>
2020-04-03 21:46:40 +0200

    alert 0.0.4, input type fix

--- projects/alert/src/lib/alert.validator.ts
+++ projects/alert/src/lib/alert.validator.ts
@@ -2,11 +2,11 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 export type InvalidDataType = (FormControl | FormArray | FormGroup | string[][])[][];
 
-export function AddInvalidControl(invalidData: InvalidDataType, control: FormControl | FormArray, error: string[]) {
+export function AddInvalidControl(invalidData: InvalidDataType, control: FormControl | FormArray | FormGroup, error: string[]) {
    invalidData.splice(0, 0, [control, [error]]);
 }
 
-export function AddInvalidError(invalidData: InvalidDataType, control: FormControl | FormArray, error: string[]) {
+export function AddInvalidError(invalidData: InvalidDataType, control: FormControl | FormArray | FormGroup, error: string[]) {
    for (const data of invalidData) {
       if (data[0] === control) {
          const index = invalidData.indexOf(data);
